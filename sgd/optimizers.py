00e0da3763301aee0b0fbca3278ee189ecc9d47e
Johan <johan.attia@capgemini.com>
2020-08-31 04:02:17 +0200

    update readme + non-updates methods in optimizers

--- sgd/optimizers.py
+++ sgd/optimizers.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import tensorflow as tf
 from tensorflow.python.training.tracking import base as trackable
 
@@ -7,7 +9,7 @@ from schedule import MomentumSchedule
 # TODO : minimizer function for TensorFlow
 
 
-class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstring, __init__, _prepare_local
+class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : _prepare_local
     """Stochastic gradient descent and momentum optimizer, including Nesterov acceleration.
     
     This implementation allows momentum schedule, see schedule.py. Concerning formulation details, see :
@@ -19,49 +21,54 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstr
             `tf.keras.optimizers.schedules.LearningRateSchedule`, or a callable
             that takes no arguments and returns the actual value to use. The
             learning rate. Defaults to 0.01.
-        momentum: float hyperparameter >= 0 that accelerates gradient descent
-            in the relevant direction and dampens oscillations. 
-            Defaults to 0, i.e., vanilla gradient descent.
+        momentum:  A `Tensor`, floating point value, or a schedule that is a
+            `MomentumSchedule`, or a callable that takes no arguments and returns the 
+            actual value to use >= 0. Accelerates gradient descent in the relevant direction 
+            and dampens oscillations. Defaults to 0, i.e., vanilla gradient descent.
         nesterov: boolean. Whether to apply Nesterov momentum.
             Defaults to `False`.
         name: Optional name prefix for the operations created when applying
-            gradients.  Defaults to `"SGD"`.
+            gradients.  Defaults to `'SGD'`.
         **kwargs: Keyword arguments. Allowed to be one of
             `"clipnorm"` or `"clipvalue"`.
             `"clipnorm"` (float) clips gradients by norm; `"clipvalue"` (float) clips
             gradients by value.
     """
     
-    def __init__(self, 
-                 learning_rate: float = 0.01, 
-                 momentum: float = 0.0, 
-                 nesterov: bool = False, 
-                 name: str = 'Momentum',
-                 **kwargs):
+    def __init__(
+        self,
+        learning_rate: Union[float, tf.keras.optimizers.schedules.LearningRateSchedule] = 0.01,
+        momentum: Union[float, MomentumSchedule] = 0.0,
+        nesterov: bool = False,
+        name: str = 'SGD',
+        **kwargs
+    ):
 
         super(MomentumScheduledSGD, self).__init__(name, **kwargs)
         self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
         self._set_hyper('decay', self._initial_decay)
 
         self._momentum = False
-        if isinstance(momentum, (tf.Tensor, MomentumSchedule)) or callable(momentum) or momentum > 0:
+        if (isinstance(momentum, 
+                       (tf.Tensor, MomentumSchedule)) 
+            or callable(momentum) or momentum > 0):
             self._momentum = True
         if isinstance(momentum, (int, float)) and (momentum < 0 or momentum > 1):
             raise ValueError("`momentum` must be between [0, 1].")
-        self._set_hyper('momentum', momentum)
         
+        self._set_hyper('momentum', momentum)
         self.nesterov = nesterov
         
     def _set_hyper(self, name, value):
         """Set hyper `name` to value. value can be callable, tensor, numeric.
-        This method is overriden to allow MomentumSchedule use for momentum hyperparameter, without loss of readability.
+        This method is overriden to allow MomentumSchedule use for momentum hyperparameter.
         """
         if isinstance(value, trackable.Trackable):
             self._track_trackable(value, name, overwrite=True)
-        
+
         if name not in self._hyper:
             self._hyper[name] = value
-
+        
         else:
             prev_value = self._hyper[name]
             if (callable(prev_value)
@@ -76,7 +83,7 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstr
     
     def _get_hyper(self, name, dtype=None):
         """Get value of hyper `name`. value can be callable, tensor, numeric.
-        This method is overriden to allow MomentumSchedule use for momentum hyperparameter, without loss of readability.
+        This method is overriden to allow MomentumSchedule use for momentum hyperparameter.
         """
         if not self._hypers_created:
             self._create_hypers()
@@ -88,7 +95,7 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstr
         
         if callable(value):
             value = value()
-
+        
         if dtype:
             return tf.cast(value, dtype)
         else:
@@ -101,6 +108,15 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstr
         if self._momentum:
             for var in var_list:
                 self.add_slot(var, 'momentum')
+
+    def _prepare_local(self, var_device, var_dtype, apply_state):
+        """ docstring...
+        """
+        # TODO : dynamic momentum
+        super(MomentumScheduledSGD, self)._prepare_local(var_device, var_dtype, apply_state)
+        apply_state[(var_device, var_dtype)]['momentum'] = tf.identity(
+            self._get_hyper('momentum', var_dtype)
+        )
             
     def _resource_apply_dense(self, grad, var, apply_state=None):
         #var_dtype = var.dtype.base_dtype
@@ -136,7 +152,7 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : class docstr
     
     def _serialize_hyperparameter(self, hyperparameter_name):
         """Serialize a hyperparameter that can be a float, callable, or Tensor.
-        This method is overriden to allow MomentumSchedule serialization, without loss of readability.
+        This method is overriden to allow MomentumSchedule serialization.
         """
         value = self._hyper[hyperparameter_name]
         if isinstance(value, 
