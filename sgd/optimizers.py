59a96018e919392888e8244d3f56412befa6a244
Johan <johan.attia@capgemini.com>
2020-09-03 00:40:46 +0200

    finalize schedule format and docstrings

--- sgd/optimizers.py
+++ sgd/optimizers.py
@@ -9,21 +9,22 @@ from schedule import MomentumSchedule
 # TODO : minimizer function for TensorFlow
 
 
-class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : _prepare_local
-    """Stochastic gradient descent and momentum optimizer, including Nesterov acceleration.
-    
+class MomentumScheduledSGD(tf.keras.optimizers.Optimizer):  # TODO : _prepare_local
+    """
+    Stochastic gradient descent and momentum optimizer, including Nesterov acceleration.
+
     This implementation allows momentum schedule, see schedule.py. Concerning formulation details, see :
     - http://proceedings.mlr.press/v28/sutskever13.pdf
     - https://arxiv.org/pdf/1212.0901.pdf
 
-    Args:
+    Arguments:
         learning_rate: A `Tensor`, floating point value, or a schedule that is a
             `tf.keras.optimizers.schedules.LearningRateSchedule`, or a callable
             that takes no arguments and returns the actual value to use. The
             learning rate. Defaults to 0.01.
         momentum:  A `Tensor`, floating point value, or a schedule that is a
-            `MomentumSchedule`, or a callable that takes no arguments and returns the 
-            actual value to use >= 0. Accelerates gradient descent in the relevant direction 
+            `MomentumSchedule`, or a callable that takes no arguments and returns the
+            actual value to use >= 0. Accelerates gradient descent in the relevant direction
             and dampens oscillations. Defaults to 0, i.e., vanilla gradient descent.
         nesterov: boolean. Whether to apply Nesterov momentum.
             Defaults to `False`.
@@ -34,31 +35,36 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : _prepare_loc
             `"clipnorm"` (float) clips gradients by norm; `"clipvalue"` (float) clips
             gradients by value.
     """
-    
+
     def __init__(
         self,
-        learning_rate: Union[float, tf.keras.optimizers.schedules.LearningRateSchedule] = 0.01,
+        learning_rate: Union[
+            float, tf.keras.optimizers.schedules.LearningRateSchedule
+        ] = 0.01,
         momentum: Union[float, MomentumSchedule] = 0.0,
         nesterov: bool = False,
-        name: str = 'SGD',
+        name: str = "SGD",
         **kwargs
     ):
 
         super(MomentumScheduledSGD, self).__init__(name, **kwargs)
-        self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
-        self._set_hyper('decay', self._initial_decay)
+        self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
+        self._set_hyper("decay", self._initial_decay)
 
         self._momentum = False
-        if (isinstance(momentum, 
-                       (tf.Tensor, MomentumSchedule)) 
-            or callable(momentum) or momentum > 0):
+        if (
+            isinstance(momentum, (tf.Tensor, MomentumSchedule))
+            or callable(momentum)
+            or momentum > 0
+        ):
             self._momentum = True
+        
         if isinstance(momentum, (int, float)) and (momentum < 0 or momentum > 1):
             raise ValueError("`momentum` must be between [0, 1].")
-        
-        self._set_hyper('momentum', momentum)
+
+        self._set_hyper("momentum", momentum)
         self.nesterov = nesterov
-        
+
     def _set_hyper(self, name, value):
         """Set hyper `name` to value. value can be callable, tensor, numeric.
         This method is overriden to allow MomentumSchedule use for momentum hyperparameter.
@@ -68,111 +74,132 @@ class MomentumScheduledSGD(tf.keras.optimizers.Optimizer): # TODO : _prepare_loc
 
         if name not in self._hyper:
             self._hyper[name] = value
-        
+
         else:
             prev_value = self._hyper[name]
-            if (callable(prev_value)
-                or isinstance(prev_value, 
-                              (tf.Tensor, int, float, 
-                               tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule))
-                or isinstance(value, 
-                              (tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule))):
+            if (
+                callable(prev_value)
+                or isinstance(
+                    prev_value,
+                    (
+                        tf.Tensor,
+                        int,
+                        float,
+                        tf.keras.optimizers.schedules.LearningRateSchedule,
+                        MomentumSchedule,
+                    ),
+                )
+                or isinstance(
+                    value,
+                    (
+                        tf.keras.optimizers.schedules.LearningRateSchedule,
+                        MomentumSchedule,
+                    ),
+                )
+            ):
                 self._hyper[name] = value
             else:
                 tf.keras.backend.set_value(self._hyper[name], value)
-    
+
     def _get_hyper(self, name, dtype=None):
         """Get value of hyper `name`. value can be callable, tensor, numeric.
         This method is overriden to allow MomentumSchedule use for momentum hyperparameter.
         """
         if not self._hypers_created:
             self._create_hypers()
-        
+
         value = self._hyper[name]
-        if isinstance(value, 
-                      (tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule)):
+        if isinstance(
+            value,
+            (tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule),
+        ):
             return value
-        
+
         if callable(value):
             value = value()
-        
+
         if dtype:
             return tf.cast(value, dtype)
         else:
             return value
-    
+
     def _create_slots(self, var_list):
-        """Initialize new variables for momentum optimizer. Default initializer generates 
+        """Initialize new variables for momentum optimizer. Default initializer generates
         tensors initialized to 0.
         """
         if self._momentum:
             for var in var_list:
-                self.add_slot(var, 'momentum')
+                self.add_slot(var, "momentum")
 
     def _prepare_local(self, var_device, var_dtype, apply_state):
-        """ docstring...
-        """
+        """docstring..."""
         # TODO : dynamic momentum
-        super(MomentumScheduledSGD, self)._prepare_local(var_device, var_dtype, apply_state)
-        apply_state[(var_device, var_dtype)]['momentum'] = tf.identity(
-            self._get_hyper('momentum', var_dtype)
+        super(MomentumScheduledSGD, self)._prepare_local(
+            var_device, var_dtype, apply_state
         )
-            
+        apply_state[(var_device, var_dtype)]["momentum"] = tf.identity(
+            self._get_hyper("momentum", var_dtype)
+        )
+
     def _resource_apply_dense(self, grad, var, apply_state=None):
-        #var_dtype = var.dtype.base_dtype
+        # var_dtype = var.dtype.base_dtype
         #
-        #lr_t = self._decayed_lr(var_dtype)
-        #m_t = self._get_hyper('momentum', var_dtype)
+        # lr_t = self._decayed_lr(var_dtype)
+        # m_t = self._get_hyper('momentum', var_dtype)
         #
-        #v = self.get_slot(var, 'momentum')
-        #v_t = v.assign(m_t * v - lr_t * grad, use_locking=self._use_locking)
+        # v = self.get_slot(var, 'momentum')
+        # v_t = v.assign(m_t * v - lr_t * grad, use_locking=self._use_locking)
         #
-        #if self.nesterov:
+        # if self.nesterov:
         #    var_update = var.assign_add(- m_t * v + (1 + m_t) * v_t, use_locking=self._use_locking)
-        #    
-        #else:
+        #
+        # else:
         #    var_update = var.assign_add(v_t, use_locking=self._use_locking)
-        #    
-        #return tf.group(*[var_update, v_t])
-        raise NotImplementedError('Not yet implemented')
-        
+        #
+        # return tf.group(*[var_update, v_t])
+        raise NotImplementedError("Not yet implemented")
+
     def _resource_apply_sparse(self, grad, var, indices, **kwargs):
-        #var_dtype = var.dtype.base_dtype
+        # var_dtype = var.dtype.base_dtype
         #
-        #lr_t = self._decayed_lr(var_dtype)
-        #m_t = self._get_hyper('momentum', var_dtype)
+        # lr_t = self._decayed_lr(var_dtype)
+        # m_t = self._get_hyper('momentum', var_dtype)
         #
-        #v = self.get_slot(var, 'momentum')
-        #v_t = v.assign(m_t * v - lr_t * grad, use_locking=self._use_locking)
+        # v = self.get_slot(var, 'momentum')
+        # v_t = v.assign(m_t * v - lr_t * grad, use_locking=self._use_locking)
         #
         #
         #
-        #return tf.group(*[var_update, v_t])
-        raise NotImplementedError('Not yet implemented')
-    
+        # return tf.group(*[var_update, v_t])
+        raise NotImplementedError("Not yet implemented")
+
     def _serialize_hyperparameter(self, hyperparameter_name):
         """Serialize a hyperparameter that can be a float, callable, or Tensor.
         This method is overriden to allow MomentumSchedule serialization.
         """
         value = self._hyper[hyperparameter_name]
-        if isinstance(value, 
-                      (tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule)):
+        if isinstance(
+            value,
+            (tf.keras.optimizers.schedules.LearningRateSchedule, MomentumSchedule),
+        ):
             return tf.keras.optimizers.schedules.serialize(value)
-        
+
         if callable(value):
             return value()
-        
+
         if tf.is_tensor(value):
             return tf.keras.backend.get_value(value)
-        
+
         return value
-    
+
     def get_config(self):
         config = super(MomentumScheduledSGD, self).get_config()
-        config.update({
-            'learning_rate': self._serialize_hyperparameter('learning_rate'),
-            'decay': self._serialize_hyperparameter('decay'),
-            'momentum': self._serialize_hyperparameter('momentum'),
-            'nesterov': self.nesterov,
-        })
+        config.update(
+            {
+                "learning_rate": self._serialize_hyperparameter("learning_rate"),
+                "decay": self._serialize_hyperparameter("decay"),
+                "momentum": self._serialize_hyperparameter("momentum"),
+                "nesterov": self.nesterov,
+            }
+        )
         return config
--- sgd/schedule.py
+++ sgd/schedule.py
@@ -3,44 +3,46 @@ import tensorflow as tf
 
 
 def log(x, base, dtype=tf.float32):
-    """Logarithm with base.
-    
+    """
+    Logarithm with base.
+
     Args:
         x: A `Tensor`.
         base: A `Tensor`.
-    
+
     Returns:
         A `Tensor` with same shape as x.
     """
-    n = tf.math.log(tf.cast(x, dtype=dtype))
-    d = tf.math.log(tf.cast(base, dtype=dtype))
+    n = tf.math.log(tf.cast(x, dtype))
+    d = tf.math.log(tf.cast(base, dtype))
 
     return tf.divide(n, d)
 
-# TODO : unify class and __init__ docstrings
 
-class MomentumSchedule(object):
-    """A serializable momentum schedule.
-    
-    When training a model, a MomentumSchedule can be passed in as the momentum 
-    of MomentumScheduledSGD, see optimizers.py.
+class MomentumSchedule:
+    """
+    A serializable momentum schedule.
+
+    When training a model, a MomentumSchedule can be passed in as the momentum
+    of the Momentum optimizer, see optimizers.py.
     """
 
     @abc.abstractmethod
     def __call__(self, step):
-        raise NotImplementedError('Momentum schedule must override __call__')
+        raise NotImplementedError("Momentum schedule must override __call__")
 
     @abc.abstractmethod
     def get_config(self):
-        raise NotImplementedError('Momentum schedule must override get_config')
+        raise NotImplementedError("Momentum schedule must override get_config")
 
     @classmethod
     def from_config(cls, config):
-        """Instantiates a MomentumSchedule class from its config.
-        
-        Args:
+        """
+        Instantiates a MomentumSchedule class from its config.
+
+        Arguments:
             config: Output of `get_config()`.
-        
+
         Returns:
             A MomentumSchedule instance.
         """
@@ -48,103 +50,99 @@ class MomentumSchedule(object):
 
 
 class ConvexSchedule(MomentumSchedule):
-    """A MomentumSchedule suitable for convex functions.
-    See http://www.cs.toronto.edu/~hinton/absps/momentum.pdf.
+    """
+    A MomentumSchedule suitable for convex functions.
+    See http://proceedings.mlr.press/v28/sutskever13.pdf.
 
     ```python
     schedule = schedules.ConvexSchedule()
     model.compile(
-        optimizer=optimizers.MomentumScheduledSGD(momentum=schedule),
+        optimizer=optimizers.Momentum(momentum=schedule),
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
     model.fit(data, labels, epochs=5)
     ```
 
+    Args:
+        const: A scalar `float32` or `float64` `Tensor` or a Python number.
+            Defaults to 1.0. The multiplicative constant.
+        name: String. Defaults to `ConvexSchedule`.
+            Optional name of the operation.
+
     Returns:
-        A 1-arg callable momentum schedule that takes the current optimizer step 
+        A 1-arg callable momentum schedule that takes the current optimizer step
         and outputs a scalar `Tensor` as momentum value.
     """
 
     def __init__(self, const: float = 1.0, name: str = None):
-        """Applies momentum schedule suitable for convex functions.
-
-        Args:
-            const: A scalar `float32` or `float64` `Tensor` or a Python number. 
-                Defaults to 1.0. The multiplicative constant.
-            name: String. Defaults to `ConvexSchedule`.
-                Optional name of the operation.
-        """
         super(ConvexSchedule, self).__init__()
+
+        if const < 0 or const > 1:
+            raise ValueError("`const` must be between [0, 1].")
+
         self.const = const
         self.name = name
 
     def __call__(self, step):
-        with tf.name_scope(self.name or 'ConvexSchedule') as name:
+        with tf.name_scope(self.name or "ConvexSchedule") as name:
             const = tf.convert_to_tensor(
-                self.const, 
-                dtype=tf.float32, 
-                name='multiplicative_constant'
+                self.const, dtype=tf.float32, name="multiplicative_constant"
             )
-            step = tf.cast(step, dtype=tf.float32)
+            step = tf.cast(step, tf.float32)
             step_value = const * 3 / (step + 5)
 
             return tf.subtract(1, step_value, name=name)
 
     def get_config(self):
-        return {
-            'const': self.const,
-            'name': self.name
-        }
+        return {"const": self.const, "name": self.name}
 
 
 class StronglyConvexSchedule(MomentumSchedule):
-    """A MomentumSchedule achieving exponential convergence on 
-    strongly convex functions, see http://www.cs.toronto.edu/~hinton/absps/momentum.pdf.
+    """
+    A MomentumSchedule achieving exponential convergence on
+    strongly convex functions, see http://proceedings.mlr.press/v28/sutskever13.pdf.
 
     ```python
     schedule = schedules.StronglyConvexSchedule(upper_momentum=0.99,)
     model.compile(
-        optimizer=optimizers.MomentumScheduledSGD(momentum=schedule),
+        optimizer=optimizers.Momentum(momentum=schedule),
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
     model.fit(data, labels, epochs=5)
     ```
 
+    Args:
+        upper_momentum:  A scalar `float32` or `float64` `Tensor` or a
+            Python number. Defaults to 0.99. The upper momentum.
+        name: String. Defaults to `StronglyConvexSchedule`.
+            Optional name of the operation.
+
     Returns:
-        A 1-arg callable momentum schedule that takes the current 
-        optimizer step and outputs the momentum value, a scalar `Tensor` of 
+        A 1-arg callable momentum schedule that takes the current
+        optimizer step and outputs the momentum value, a scalar `Tensor` of
         the same type as `upper_momentum`.
     """
 
     def __init__(self, upper_momentum: float = 0.99, name: str = None):
-        """Applies momentum schedule achieving exponential convergence on strongly convex functions.
-
-        Args:
-            upper_momentum:  A scalar `float32` or `float64` `Tensor` or a 
-                Python number. Defaults to 0.99. The upper momentum.
-            name: String. Defaults to `StronglyConvexSchedule`.
-                Optional name of the operation.
-        """
         super(StronglyConvexSchedule, self).__init__()
+
+        if upper_momentum < 0 or upper_momentum > 1:
+            raise ValueError("`upper momentum` must be between [0, 1].")
+
         self.upper_momentum = upper_momentum
         self.name = name
 
     def __call__(self, step):
-        with tf.name_scope(self.name or 'StronglyConvexSchedule') as name:
+        with tf.name_scope(self.name or "StronglyConvexSchedule") as name:
             upper_momentum = tf.convert_to_tensor(
-                self.upper_momentum,
-                dtype=tf.float32,
-                name='upper_momentum'
+                self.upper_momentum, dtype=tf.float32, name="upper_momentum"
             )
-            step = tf.cast(step, dtype=tf.float32)
-            step_value = 1 - tf.pow(2., -1 - log(tf.floor(step/250) + 1, 2))
+            step = tf.cast(step, tf.float32)
+            step_value = 1 - tf.pow(2.0, -1 - log(tf.floor(step / 250) + 1, 2))
 
             return tf.minimum(step_value, upper_momentum, name=name)
 
     def get_config(self):
-        return {
-            'upper_momentum': self.upper_momentum,
-            'name': self.name
-        }
+        return {"upper_momentum": self.upper_momentum, "name": self.name}
