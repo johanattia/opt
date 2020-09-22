caa5976d3d9f37f0a6d6c8ba169c469675de3ce2
Johan <johan.attia@capgemini.com>
2020-09-23 00:39:57 +0200

    tf control flow in minimizer

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # opt :dart:
-This repository aims to provide implementations of modern optimization algorithms for machine learning in [TensorFlow 2.x](https://github.com/tensorflow/tensorflow), [JAX](https://jax.readthedocs.io/en/latest/) and [Flax](https://flax.readthedocs.io/en/latest/), with the goal to reproduce experiments from the published research articles.
+This repository aims to provide implementations of modern optimization algorithms for machine learning in [TensorFlow 2.x](https://github.com/tensorflow/tensorflow), [Jax](https://jax.readthedocs.io/en/latest/) and [Trax](https://trax-ml.readthedocs.io/en/latest/), with the goal to reproduce experiments from the published research articles.
 
 
 ## Resume
--- opt/minimize.py
+++ opt/minimize.py
@@ -2,9 +2,6 @@ from typing import Callable, Optional
 import tensorflow as tf
 
 
-# TODO : tf.while_loop
-
-
 def minimizer(
     func: Callable,
     optimizer: tf.keras.optimizers.Optimizer,
@@ -22,31 +19,39 @@ def minimizer(
         initial_position = tf.Variable(initial_position)
         comparing_position = tf.Variable(initial_position)
 
-    history = dict(value_function=[], antecedent=[])
+    history = dict(function_value=[], position_value=[])
     iterations = tf.Variable(0)
 
-    with tf.name_scope(name or "minimize"):
-        while tf.reduce_all(
-            tf.less_equal(func(comparing_position), func(initial_position))
-        ):
+    with tf.name_scope(name or "minimizer"):
+
+        def _cond(initial_position, comparing_position):
+            return tf.reduce_all(
+                tf.less_equal(func(comparing_position), func(initial_position))
+            )
+
+        def _body(initial_position, comparing_position):
             initial_position.assign(comparing_position.value())
+            cost_function = lambda: func(comparing_position)
+            optimizer.minimize(cost_function, [comparing_position])
 
-            with tf.GradientTape() as tape:
-                value_function = func(comparing_position)
-
-            grad = tape.gradient(value_function, [comparing_position])
-            optimizer.apply_gradients(zip(grad, [comparing_position]))
-
-            history["value_function"].append(func(initial_position).numpy())
-            history["antecedent"].append(initial_position.numpy())
+            history["function_value"].append(func(initial_position).numpy())
+            history["position_value"].append(initial_position.numpy())
             iterations.assign_add(1)
 
             tf.print(
-                "Iterations :",
+                "Iteration",
                 iterations,
-                "- Value function :",
-                history["value_function"][-1],
-                "- Antecedent :",
-                history["antecedent"][-1],
+                "- Function value :",
+                history["function_value"][-1],
+                "- Position value :",
+                history["position_value"][-1],
             )
+            return initial_position, comparing_position
+
+        tf.while_loop(
+            cond=_cond,
+            body=_body,
+            loop_vars=[initial_position, comparing_position],
+            shape_invariants=[initial_position.shape, comparing_position.shape],
+        )
         return history
